11b33ed041133ca57df5c6388dbf629bc74146dc
Anderson V. Santos <47935177+ladeepweb@users.noreply.github.com>
2019-09-17 16:16:33 +0200

    Update irc.php

--- irc.php
+++ irc.php
@@ -1,5 +1,6 @@
 <?php
-
+// SCRIPTS
+require('scripts/4devs.php');
 // TIME ZONE
 date_default_timezone_set('America/Santiago');
 
@@ -200,30 +201,10 @@ while ( is_resource( $socket ) ) {
     
       }
       if ( $d[3] === ':!random' ) {
-      function GeraPessoa(){
-    $headers = array( 
-            "POST /ferramentas_online.php HTTP/1.0", 
-            "Content-type: application/x-www-form-urlencoded; charset=UTF-8", 
-            "Accept: application/json", 
-            "Origin: https://www.4devs.com.br", 
-        ); 
-    $idade  = rand(20,50);
-    $data = 'acao=gerar_pessoa&cep_cidade=&cep_estado=&idade='.$idade.'&pontuacao=S';
-    $curl = curl_init();
-    curl_setopt($curl, CURLOPT_URL, "https://www.4devs.com.br/ferramentas_online.php");
-    curl_setopt($curl, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36");
-    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
-    curl_setopt($curl, CURLOPT_POST, 1);
-    curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-    $output = curl_exec($curl);
-    curl_close($curl);
-    return $output;
-}
-$random = json_decode(GeraPessoa());
-$resposta = "[ChkRANDOM] → [NOME] $dados->nome [CPF] $dados->cpf [RG] $dados->rg [NASCIMENTO] $dados->data_nasc [CEP] $dados->cep [RUA] $dados->endereco, $dados->numero [BAIRRO] $dados->bairro [CIDADE] $dados->cidade [ESTADO] $dados->estado [TELEFONE] $dados->celular\n";
+
+     $dados = json_decode(GeraPessoa());
+
+     $resposta = "07[ChkRANDOM] →02 [NOMBRE] $dados->nome  04| [CPF] $dados->cpf  04| [RG] $dados->rg 04| [NACIMIENTO] $dados->data_nasc 04| [CEP] $dados->cep 04| [RUA] $dados->endereco, $dados->numero 04| [BAIRRO] $dados->bairro 04| [CIDAD] $dados->cidade 04| [ESTADO] $dados->estado 04| [TELÉFONO] $dados->celular\n";
 
  // ENVIANDO RESPOSTA AO IRC
    socket_write($socket,'PRIVMSG '.$d[2]." :$resposta\r\n" );
